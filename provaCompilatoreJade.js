f6d4e54244c68e9949c70bc7dbabc7bd6ac544e5
flavio-a <flavio.a.618@gmail.com>
2015-10-21 09:47:56 +0200

    Aggiunto script per compilare tutte le pagine

--- provaCompilatoreJade.js
+++ provaCompilatoreJade.js
@@ -1,12 +1,34 @@
 var jade = require('jade');
 var fs   = require('fs');
 
-//console.log(fs.readdirSync('./jade'));
+exploreFolder('./jade', compileJade);
 
-compileJade('./jade/pages/Pokedex_strategico.jade', './pages/Pokedex_strategico.html');
-compileJade('./jade/index.jade', './index.html');
-compileJade('./jade/pages/Pokemon/Alakazam.jade', './pages/Pokemon/Alakazam.html');
-compileJade('./jade/pages/Pokemon/Dragonite.jade', './pages/Pokemon/Dragonite.html');
+function exploreFolder(folder, work){
+	fs.readdir(folder, function (err, list){
+		for (var path in list){
+			var fullPath = folder + '/' + list[path];
+			if (list[path].indexOf('.') == -1)
+				exploreFolder(fullPath, work);
+			else if (isPage(fullPath)) {
+				work(fullPath, getSavePath(fullPath));
+			}
+		}
+	});
+}
+
+function isPage(path){
+	if (path.indexOf('templates') === -1)
+		if (path.indexOf('.jade') === (path.length - '.jade'.length))
+		return true;
+	return false;
+}
+
+function getSavePath(path){
+	var savePath = path;
+	savePath = '.' + savePath.substring('./jade'.length);
+	savePath = savePath.substring(0, savePath.length - '.jade'.length) + '.html';
+	return savePath;
+}
 
 //compile the jade file in 'readPath' and save it in 'savePath'
 function compileJade(readPath, savePath){
